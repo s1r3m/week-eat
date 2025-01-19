ef3c9d7b4eb7025a90c5276e62b11545bcb96141
Filipp Aleksandrov <f_aleksandrov@wargaming.net>
2025-01-19 18:04:35 +0200

    WEP-10: add main tests

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,5 +1,21 @@
+from fastapi.testclient import TestClient
+import pytest
+
 from week_eat_planner.config import settings
+from week_eat_planner.main import app
 
 
-async def test_main__always__success():
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+def test_settings__always__settings_present():
     assert settings
+
+
+def test_app_instance__bad_path__not_found_response(client):
+    response = client.get("/test")
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Not Found"}
