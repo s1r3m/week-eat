331ec16a65c3b056ccf38fab8c9c3e3df9132434
Filipp Aleksandrov <f_aleksandrov@wargaming.net>
2025-02-02 16:46:51 +0200

    WEP-10: add base DAO unittests

---
+++ backend/tests/db/test_base_dao.py
@@ -0,0 +1,135 @@
+import asyncio
+import pytest
+import pytest_asyncio
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from week_eat_planner.dao.base import BaseDAO
+from week_eat_planner.dao.database import Base
+
+
+class TestModel(Base):
+    __tablename__ = 'test'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __eq__(self, other: 'TestModel') -> bool:
+        return self.id == other.id and self.name == other.name
+
+
+class TestDAO(BaseDAO):
+    model = TestModel
+
+
+@pytest_asyncio.fixture
+async def mocked_session(mocker) -> AsyncSession:
+    return mocker.AsyncMock(spec=AsyncSession)
+
+
+@pytest_asyncio.fixture(autouse=True, scope='module')
+def loop():
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture
+def test_model():
+    return TestModel(id=1, name='test')
+
+
+@pytest.fixture
+def mocked_record(mocked_session, mocker, test_model):
+    mocked_result = mocker.MagicMock()
+    mocked_result.scalar_one_or_none.return_value = test_model
+    mocked_session.execute.return_value = mocked_result
+
+    return test_model
+
+
+@pytest.fixture
+def mocked_record_none(mocked_session, mocker, test_model):
+    mocked_result = mocker.MagicMock()
+    mocked_result.scalar_one_or_none.return_value = None
+    mocked_session.execute.return_value = mocked_result
+
+    return test_model
+
+
+@pytest.fixture
+def mocked_filter(mocker, mocked_record):
+    mock_filter = mocker.MagicMock()
+    mock_filter.model_dump.return_value = {'id': mocked_record.id}
+
+    return mock_filter
+
+
+@pytest.fixture
+def mocked_filter_none(mocker, mocked_session):
+    mock_filter = mocker.MagicMock()
+    mock_filter.model_dump.return_value = {}
+    mocked_result = mocker.MagicMock()
+    mocked_session.execute.return_value = mocked_result
+    mocked_result.scalar_one_or_none.return_value = None
+
+    return mock_filter
+
+
+@pytest.fixture
+def pydantic_model(mocker, test_model):
+    model = mocker.MagicMock()
+    model.model_dump.return_value = {'id': test_model.id, 'name': test_model.name}
+
+    return model
+
+
+async def test_get_one_or_none_by_id__record_exists__record_returned(mocked_session, mocked_record):
+    result = await TestDAO.get_one_or_none_by_id(mocked_session, mocked_record.id)
+    assert result == mocked_record
+
+
+async def test_get_one_or_none_by_id__record_not_exists__none_returned(mocked_session, mocked_record_none):
+    result = await TestDAO.get_one_or_none_by_id(mocked_session, mocked_record_none.id)
+    assert result is None
+
+
+async def test_get_one_or_none_by_id__sql_error__exception_raised(mocked_session):
+    mocked_session.execute.side_effect = SQLAlchemyError('SQL error')
+    with pytest.raises(SQLAlchemyError) as exc:
+        await TestDAO.get_one_or_none_by_id(mocked_session, 1)
+    assert str(exc.value) == 'SQL error'
+
+
+async def test_get_one_or_none__record_exists__record_returned(mocked_session, mocked_filter, mocked_record):
+    result = await TestDAO.get_one_or_none(mocked_session, mocked_filter)
+    assert result == mocked_record
+    mocked_session.execute.assert_called_once()
+
+
+async def test_get_one_or_none__record_not_exists__none_returned(mocked_session, mocked_filter_none):
+    result = await TestDAO.get_one_or_none(mocked_session, mocked_filter_none)
+    assert result is None
+
+
+async def test_get_one_or_none__sql_error__exception_raised(mocked_session, mocked_filter_none):
+    mocked_session.execute.side_effect = SQLAlchemyError('SQL error')
+    with pytest.raises(SQLAlchemyError) as exc:
+        await TestDAO.get_one_or_none(mocked_session, mocked_filter_none)
+    assert str(exc.value) == 'SQL error'
+
+
+async def test_add__valid_obj__exception_raised(mocked_session, pydantic_model, test_model):
+    result = await TestDAO.add(mocked_session, pydantic_model)
+    assert result == test_model
+
+
+async def test_add__sql_error__exception_raised(mocked_session, pydantic_model):
+    mocked_session.add.side_effect = SQLAlchemyError('SQL error')
+
+    with pytest.raises(SQLAlchemyError) as exc:
+        await TestDAO.add(mocked_session, pydantic_model)
+
+    assert str(exc.value) == 'SQL error'
+    mocked_session.rollback.assert_awaited_once()
